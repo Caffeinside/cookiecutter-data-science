c39be32efa454054dd7c082dcdc8436da6f31d9c
b.saintot <bsaintot@octo.com>
2020-01-10 17:30:33 +0100

    Modify MakeFile

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,10 +1,12 @@
 from setuptools import find_packages, setup
-
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='{{ cookiecutter.description }}',
+    name='{{ cookiecutter.project_name }}',
     author='{{ cookiecutter.author_name }}',
-    license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
+    description='{{ cookiecutter.description }}',
+    packages=find_packages(),
+    setup_requires=['setuptools_scm'],
+    install_requires=[], # replace with useful packages in requirements.txt
+    extras_require={'tests': ['pytest', 'pytest-mock']},
+    version='0.1.0',
+    license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}'
 )
